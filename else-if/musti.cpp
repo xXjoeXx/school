85814d48276dc62782fe2b02e904a528fc9058c1
xXjoeXx <iadt34@gmail.com>
2014-11-14 23:57:59 +0100

    Create musti.cpp

---
+++ else-if/musti.cpp
@@ -0,0 +1,30 @@
+/*
+1. Aufgabe.
+Als eine Variable bezeichnet man einen Platzhalter. Dieser kann je nach Typ
+unterschiedliche Zahlen oder Buchstaben oder sogar alphanumerische Kombinationen
+beinhalten.
+Eine Variable des Typs integer speichert lediglich Zahlen in einem gewissen berreich.
+Weiter beschränkt sich dieser Datentyp auf Ganzzahlige Werte.
+2. Aufgabe
+cin>>zahl;
+
+3. Aufgabe
+cout<<"Geben Sie einen Wert ein"<<endl;
+
+4. Aufgabe
+
+if (zahl == 0){
+%Anweisung%
+}
+
+5. Aufgabe
+
+zahl = 0;
+
+6. Aufgabe
+	erg1:12
+	erg2:35
+	erg3:a=5
+
+
+*/
