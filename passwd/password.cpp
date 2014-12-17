59e0aba4d181ec399806a5ce3ce8bc62d8a4d927
xXjoeXx <iadt34@gmail.com>
2014-12-17 17:13:30 +0100

    Update password.cpp
    0.2 BEEP IS WORKING!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- passwd/password.cpp
+++ passwd/password.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
 #include <string>
 #include <windows.h> //lustige windows beeps :O
+#include <fstream>
 using namespace std;
 
 int main(void) {
-string password="letmein", eingabe;
+string password, eingabe;
 //eingabe der datei
 
+ifstream inFile;
+inFile.open("pw.txt");
+inFile >> password;
   for ( int i = 0; i <= 2; i++) { //max. 3 falsche Eingaben
     cout << "Passwort:";
     if(cin>>eingabe, password == eingabe) {
